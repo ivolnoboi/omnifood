bf191a5b0f9757dd90f0d6d3d9f31503c3b0ba52
ivolnoboi <irinavolnoboy@gmail.com>
2022-06-16 22:19:46 +0300

    Sticky navigation has been added

--- js/script.js
+++ js/script.js
@@ -39,6 +39,29 @@ allLinks.forEach(function (link) {
 });
 
 ///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+    function (entries) {
+        const ent = entries[0];
+        const bodyClasses = document.body.classList;
+        if (!ent.isIntersecting) {
+            bodyClasses.add("sticky");
+        } else {
+            bodyClasses.remove("sticky");
+        }
+    },
+    {
+        // In the viewport
+        root: null,
+        threshold: 0,
+        rootMargin: "-80px",
+    }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
     var flex = document.createElement("div");
